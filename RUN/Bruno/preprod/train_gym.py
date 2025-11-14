08241bc5c508060abbb8174993e5c847e6d14769
Mauricio Vieira <mauricio.vieira@fing.edu.uy>
2025-11-14 10:38:56 -0300

    utils + H_iterator

--- RUN/Bruno/preprod/train_gym.py
+++ RUN/Bruno/preprod/train_gym.py
@@ -3,19 +3,28 @@ import torch
 from stable_baselines3 import PPO
 from gnn_policy import GNNActorCriticPolicy
 from envs import APNetworkEnv
+from utils import load_channel_matrix
 
 
-def channel_iterator(num_graphs=1000, n_APs=5):
-    for _ in range(num_graphs):
-        H = np.random.rand(n_APs, n_APs).astype(np.float32)
-        np.fill_diagonal(H, H.diagonal() * 2)
-        yield H
-
+# def channel_iterator(num_graphs=1000, n_APs=5):
+#     for _ in range(num_graphs):
+#         H = np.random.rand(n_APs, n_APs).astype(np.float32)
+#         np.fill_diagonal(H, H.diagonal() * 2)
+#         yield H
 
 def train_gnn_policy():
     n_APs = 5
     num_channels = 3
     n_power_levels = 2
+
+    H_iterator = load_channel_matrix(
+            building_id=990,
+            b5g=False,  # 2.4 GHz
+            num_links=n_APs,
+            synthetic=False,  # Cambiar a True para usar datos sintéticos
+            shuffle=True,
+            repeat=True  # Reinicia automáticamente el iterador
+        )
 
     env = APNetworkEnv(
         n_APs=n_APs,
@@ -24,7 +33,7 @@ def train_gnn_policy():
         P0=4,
         Pmax=0.7,
         max_steps=50,
-        H_iterator=channel_iterator(10000, n_APs)
+        H_iterator=H_iterator
     )
 
     policy_kwargs = dict(gnn_hidden_dim=32,
